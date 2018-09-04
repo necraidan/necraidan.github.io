6977e2f14d9c0d2f6706292856d364bdfcb9a14d
BuildTools <benjamin.auzanneau.tm@gmail.com>
2018-09-04 22:30:53 +0200

    Add new scripts for the after deploy sequence.

---
+++ scripts/afterDeploy.sh
@@ -0,0 +1,6 @@
+echo \"necraidan.com\" >> dist/CNAME
+
+echo "# necraidan build [![Build Status](https://travis-ci.org/necraidan/necraidan.github.io.svg?branch=dev)](https://travis-ci.org/necraidan/necraidan.github.io)
+---------
+Built from dev branch.
+" >> dist/README.md
