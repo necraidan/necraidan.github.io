f2d8e28b2c0dfc61ef8665e0c1ec00b1092d3a69
BuildTools <benjamin.auzanneau.tm@gmail.com>
2019-02-23 22:19:07 +0100

    Change the konami code

--- src/app/shared/directive/konami.directive.ts
+++ src/app/shared/directive/konami.directive.ts
@@ -1,33 +1,33 @@
-import { EventEmitter, Directive, Output, HostListener } from '@angular/core';
+import { Directive, EventEmitter, HostListener, Output } from '@angular/core';
 
 @Directive({
-    // tslint:disable-next-line:directive-selector
-    selector: '[konami]'
+  // tslint:disable-next-line:directive-selector
+  selector: '[konami]'
 })
 export class KonamiDirective {
-    @Output() konami: EventEmitter<void> = new EventEmitter();
+  @Output() konami: EventEmitter<void> = new EventEmitter();
 
-    private seq: string[] = [];
+  private seq: string[] = [];
 
-    private konamiCode: string[] = ['38', '38', '40', '40', '37', '39', '37', '39', '66', '65'];
+  private konamiCode: string[] = ['38', '38', '40', '40', '37', '39', '37', '39', '66', '65'];
 
-    constructor() {}
+  constructor() {}
 
-    // tslint:disable-next-line:no-unused-expression
-    @HostListener('document:keydown', ['$event'])
-    handleKeyboardEvent(event: KeyboardEvent) {
-        if (event.keyCode) {
-            this.seq.push(event.keyCode.toString());
+  // tslint:disable-next-line:no-unused-expression
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    if (event.keyCode) {
+      this.seq.push(event.keyCode.toString());
 
-            // tslint:disable:no-unused-expression
-            this.seq.length > this.konamiCode.length && this.seq.shift();
+      // tslint:disable:no-unused-expression
+      this.seq.length > this.konamiCode.length && this.seq.shift();
 
-            this.isKonamiCode() && this.konami.emit();
-        }
+      this.isKonamiCode() && this.konami.emit();
     }
-    // tslint:enable:no-unused-expression
+  }
+  // tslint:enable:no-unused-expression
 
-    isKonamiCode(): boolean {
-        return this.konamiCode.every((code: string, index: number) => code === this.seq[index]);
-    }
+  isKonamiCode(): boolean {
+    return this.konamiCode.every((code: string, index: number) => code === this.seq[index]);
+  }
 }
