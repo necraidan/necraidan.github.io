1547d62599483e7d4423e0d7cd16fbcf1417fe25
BuildTools <benjamin.auzanneau.tm@gmail.com>
2021-04-12 01:27:26 +0200

    Add some style

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { WHO_AM_I } from './main.constants';
 
 @Component({
   selector: 'nec-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class MainComponent {
+  WHO_AM_I = WHO_AM_I;
 }
---
+++ src/app/main/main.constants.ts
@@ -0,0 +1,1 @@
+export const WHO_AM_I: String[] = ['Développeur Front-End', 'Artistan du code', 'Créateur de contenu', 'Speaker', 'Podcasteur'];
