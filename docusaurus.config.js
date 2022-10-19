cb4c0ee82d963719c96bcf91777b0b58af87d0e4
Benjamin Auzanneau <benjamin.auzanneau.tm@gmail.com>
2022-10-19 14:42:28 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "necraidan",
-  tagline: "Dinosaurs are cool",
+  tagline: "Developper.",
   url: "https://necraidan.com",
   baseUrl: "/",
   onBrokenLinks: "throw",
